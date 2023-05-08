109f90a0e098dbff7aab0c361b62a0dd06040fcf
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-08 14:11:28 -0700

    wired up fall back message in server.js

--- api/server.js
+++ api/server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use("*", (req, res) => {
+    res.status(404).json({
+        message: 'page not found',
+    })
+})
+
 module.exports = server;
